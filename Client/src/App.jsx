a4b504829d35ceef247c3eb4de607a159af55cc7
NiKHIL NAIR <nikhilnair340@gmail.com>
2024-01-28 15:28:56 +0530

    setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -20,7 +19,7 @@ function App() {
       <h1 className="text-xl font-bold text-red-800">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} 
+          count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
